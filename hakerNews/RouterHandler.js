dd8f946b49ad513347f6de4167d948c3eac1af5a
M Soulim <65006383+souli2020@users.noreply.github.com>
2022-07-04 20:54:25 +0100

    Update RouterHandler.js

--- hakerNews/RouterHandler.js
+++ hakerNews/RouterHandler.js
@@ -1,18 +1,23 @@
-import Stories from './pages/Stories.js';
-
+import stories from './pages/stories.js';
+import Item from "./pages/Item.js";
+import Favorites from './pages/Favorites.js';
+ 
+import Story from "../components/Story.js"
 const router = new Navigo( null, true, '#');
+   
  
 export default function RouterHandler () {
 
     const routes = [
-      { path: '/', page: Stories },
-       { path: '/', page: Stories },
-      { path: '/new', page: Stories },
-      { path: '/ask', page: Stories },
-      { path: '/show', page: Stories },
-       
-      
         
+        { path: '/', page: stories },
+        { path: '/', page: stories },
+        { path: '/new', page: stories },
+        { path: '/ask', page: stories },
+        { path: '/show', page: stories },
+        { path: '/item', page: Item },
+         { path: '/favorites', page: Favorites }
+      
     ];
      
     routes.forEach((route) => {
